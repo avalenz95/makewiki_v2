8d87e48f8c667c7e2f0112d49720f62a26572888
ablades <audaris@gmail.com>
2019-11-14 14:11:51 -0800

    remove url hardcoding

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from accounts.views import SignUpView
 urlpatterns = [
 
     # Accounts
-    path('/signup', SignUpView.as_view(), name='sign-up-page'),
+    path('signup', SignUpView.as_view(), name='sign-up-page'),
 
 ]
